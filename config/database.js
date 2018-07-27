6103c9b85defa00f58ed5bc7e929ebc032f8e681
Amr Soliman <soliman.amr@gmail.com>
2018-07-27 19:59:41 +0200

    123123

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://Amr_Soliman:Toti60652@cluster0-zyenb.mongodb.net/test?retryWrites=true' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb+srv://Amr_Soliman:Toti60652@cluster0-zyenb.mongodb.net:27017/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
