1ec8f49b47374ba53c323a9195e97413a5901230
Amr Soliman <soliman.amr@gmail.com>
2018-07-28 12:12:04 +0200

    q

--- server.js
+++ server.js
@@ -4,11 +4,10 @@
 // get all the tools we need
 var express  = require('express');
 var app      = express();
-var port     = process.env.PORT || 8080;
+var port     = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
-
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
